b2bf09e1673d0b788539391c1ae14c2200f6496f
nutpathakorn <nutpathakorn@gmail.com>
2020-04-21 08:32:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -136,7 +136,7 @@ if (!is_null($events['events'])) {
 					}
 					else{
 
-						$text = 'ขออภัยครับเกิดปัญหาบางประการขณะดำเนินการแจ้งปัญหา รบกวนให้ทำการแจ้งปัญหาอีกครั้งครับ : $empcode='.$empcode.' emp_name='.$empnametest.' empdept='.$empdept.' empmail='.$empmail.' luserid='.$luserid;
+						$text = 'ขออภัยครับเกิดปัญหาบางประการขณะดำเนินการแจ้งปัญหา รบกวนให้ทำการแจ้งปัญหาอีกครั้งครับ : $empcode='.$empcode.' emp_name='.$empname.' empdept='.$empdept.' empmail='.$empmail.' luserid='.$luserid;
 						//$text = 'ขออภัยครับเกิดปัญหาบางประการขณะดำเนินการแจ้งปัญหา รบกวนให้ทำการแจ้งปัญหาอีกครั้งครับ';
 						$messages = [
 						'type' => 'text',
