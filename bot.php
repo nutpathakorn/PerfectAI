8285c01bd00c927d0f6cbf9b825983faee459ee6
nutpathakorn <nutpathakorn@gmail.com>
2020-04-22 08:23:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -223,8 +223,8 @@ if (!is_null($events['events'])) {
 			else if($tgreed5 !== false){
 				$messages = [
 				'type' => 'image',
-				'originalContentUrl': 'https://drive.google.com/open?id=1DI0nXLWCZf_zYUsCYveZ8JWlhKFwH1TG',
-    				'previewImageUrl': 'https://drive.google.com/open?id=1DI0nXLWCZf_zYUsCYveZ8JWlhKFwH1TG',
+				'originalContentUrl' => 'https://drive.google.com/open?id=1DI0nXLWCZf_zYUsCYveZ8JWlhKFwH1TG',
+    				'previewImageUrl' => 'https://drive.google.com/open?id=1DI0nXLWCZf_zYUsCYveZ8JWlhKFwH1TG',
 				];
 			}
 			else if ($ctext == 'ขอid') {
