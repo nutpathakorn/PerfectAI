38253987c256c0d07cb3813059e989e2ec5c2d4d
nutpathakorn <nutpathakorn@gmail.com>
2020-04-22 08:22:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -184,12 +184,6 @@ if (!is_null($events['events'])) {
 			$tgreed3 = stripos($ctext, 'ช่วยด้วย');
 			$tgreed4 = stripos($ctext, 'wfh');
 			$tgreed5 = stripos($ctext, 'ตารางงาน');
-			
-			"line": {
-    "type": "image",
-    "originalContentUrl": "https://1.bp.blogspot.com/-U90M8DyKu7Q/W9EtONMCf6I/AAAAAAAAW_4/7L_jB_Rg9oweu2HKhULNdu9WNefw9zf9wCLcBGAs/s1600/sao-full.jpg",
-    "previewImageUrl": "https://3.bp.blogspot.com/-POLCd-KKazc/W9EtNxsqwpI/AAAAAAAAW_0/c8P1A4Ik3tMsCXZwaI1B2n3eXZqG0ifzwCLcBGAs/s1600/sao-preview.jpg"
-  }
 
 			if($ctext == 'สวัสดี'){
 				$text = 'สวัสดีครับ ผม PerfectAI เป็นระบบรับแจ้งปัญหาอัตโนมัติครับผม :)';
