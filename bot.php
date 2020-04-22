a48f1c9c10c416cd6ac63b38b1cae70e85dcd9cb
nutpathakorn <nutpathakorn@gmail.com>
2020-04-22 08:50:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -223,8 +223,8 @@ if (!is_null($events['events'])) {
 			else if($tgreed5 !== false){
 				$messages = [
 				'type' => 'image',
-				'originalContentUrl' => 'https://drive.google.com/file/d/1DI0nXLWCZf_zYUsCYveZ8JWlhKFwH1TG/view?usp=sharing',
-    				'previewImageUrl' => 'https://drive.google.com/file/d/1DI0nXLWCZf_zYUsCYveZ8JWlhKFwH1TG/view?usp=sharing',
+				'originalContentUrl' => 'https://raw.githubusercontent.com/nutpathakorn/RevonesQ/master/shift_it.jpg',
+    				'previewImageUrl' => 'https://raw.githubusercontent.com/nutpathakorn/RevonesQ/master/shift_it.jpg',
 				];
 			}
 			else if ($ctext == 'ขอid') {
